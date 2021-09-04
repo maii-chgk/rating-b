470395c10b0b09e918d6a53201070c9ea8d6330a
eignatenkov <egor@simplaex.com>
2021-09-04 17:25:33 +0200

    add TeamRating.add_new_teams + corresponding refactoring

--- egor/players.py
+++ egor/players.py
@@ -1,4 +1,5 @@
 from rating_api.release import get_players_release
+from egor.tools import calc_tech_rating
 import pandas as pd
 
 
@@ -13,3 +14,10 @@ class PlayerRating:
             self.data = raw_rating.set_index('player_id')
         elif file_path:
             self.data = pd.DataFrame.from_csv(file_path, index_col=0)
+
+    def calc_rt(self, player_ids, q=None):
+        """
+        вычисляет тех рейтинг по списку id игроков
+        """
+        prs = self.data.rating.reindex(player_ids).fillna(0).values
+        return calc_tech_rating(prs, q)
