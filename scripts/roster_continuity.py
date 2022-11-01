b6b8fb1aff2f2489ac27d258c2e0c2b7d1021197
Jury Razumau <jury.razumau@zendesk.com>
2022-11-01 12:30:36 +0100

    test old rules

--- scripts/roster_continuity.py
+++ scripts/roster_continuity.py
@@ -1,6 +1,5 @@
 import datetime
 from abc import ABC, abstractmethod
-from scripts import tools
 
 FIRST_DATE_OF_2021_RULES = datetime.date(2021, 8, 28)
 FIRST_DATE_OF_2022_RULES = datetime.date(2022, 10, 29)
@@ -8,22 +7,22 @@ FIRST_DATE_OF_2022_RULES = datetime.date(2022, 10, 29)
 
 class RosterContinuity(ABC):
     @abstractmethod
-    def counts(self, base_players: int, legionnaires: int, base_name_used: bool):
+    def counts(self, base_players: int, legionnaires: int, base_name_used: bool = True):
         pass
 
 
 class Pre2021Rule(RosterContinuity):
-    def counts(self, base_players: int, legionnaires: int, base_name_used: bool):
+    def counts(self, base_players: int, legionnaires: int, base_name_used: bool = True):
         return (base_players >= 4) | (base_players == 3 & base_name_used)
 
 
 class MAIIRule2021to2022(RosterContinuity):
-    def counts(self, base_players: int, legionnaires: int, base_name_used: bool):
+    def counts(self, base_players: int, legionnaires: int, base_name_used: bool = True):
         return base_players >= 4
 
 
 class MAIIRuleFrom2022(RosterContinuity):
-    def counts(self, base_players: int, legionnaires: int, base_name_used: bool):
+    def counts(self, base_players: int, legionnaires: int, base_name_used: bool = True):
         return (base_players >= 3) & (legionnaires < base_players) & (legionnaires <= 3)
 
 
--- tests/test_roster_continuity.py
+++ tests/test_roster_continuity.py
@@ -15,6 +15,54 @@ class TestRosterContinuity(unittest.TestCase):
         late_2022_rule = select_rule(datetime.date(2022, 12, 1))
         self.assertIsInstance(late_2022_rule, MAIIRuleFrom2022)
 
+    def test_old_rule_more_than_four_base_players(self):
+        self.assertTrue(Pre2021Rule().counts(6, 0))
+        self.assertTrue(Pre2021Rule().counts(6, 1))
+        self.assertTrue(Pre2021Rule().counts(6, 2))
+        self.assertTrue(Pre2021Rule().counts(6, 3))
+        self.assertTrue(Pre2021Rule().counts(5, 0))
+        self.assertTrue(Pre2021Rule().counts(5, 1))
+        self.assertTrue(Pre2021Rule().counts(5, 2))
+        self.assertTrue(Pre2021Rule().counts(5, 3))
+        self.assertTrue(Pre2021Rule().counts(4, 0))
+        self.assertTrue(Pre2021Rule().counts(4, 1))
+        self.assertTrue(Pre2021Rule().counts(4, 2))
+        self.assertTrue(Pre2021Rule().counts(4, 3))
+        self.assertTrue(Pre2021Rule().counts(4, 4))
+
+    def test_old_rule_three_players_different_name(self):
+        self.assertFalse(Pre2021Rule().counts(3, 0, False))
+        self.assertFalse(Pre2021Rule().counts(3, 1, False))
+        self.assertFalse(Pre2021Rule().counts(3, 2, False))
+        self.assertFalse(Pre2021Rule().counts(3, 3, False))
+
+    def test_old_rule_three_players_base_name(self):
+        self.assertTrue(Pre2021Rule().counts(3, 0, True))
+        self.assertTrue(Pre2021Rule().counts(3, 1, True))
+        self.assertTrue(Pre2021Rule().counts(3, 2, True))
+        self.assertTrue(Pre2021Rule().counts(3, 3, True))
+
+    def test_old_rule_not_enough_players(self):
+        self.assertFalse(Pre2021Rule().counts(0, 1))
+        self.assertFalse(Pre2021Rule().counts(0, 2))
+        self.assertFalse(Pre2021Rule().counts(0, 3))
+        self.assertFalse(Pre2021Rule().counts(0, 4))
+        self.assertFalse(Pre2021Rule().counts(0, 5))
+        self.assertFalse(Pre2021Rule().counts(0, 6))
+        self.assertFalse(Pre2021Rule().counts(1, 0))
+        self.assertFalse(Pre2021Rule().counts(1, 1))
+        self.assertFalse(Pre2021Rule().counts(1, 2))
+        self.assertFalse(Pre2021Rule().counts(1, 3))
+        self.assertFalse(Pre2021Rule().counts(1, 4))
+        self.assertFalse(Pre2021Rule().counts(1, 5))
+        self.assertFalse(Pre2021Rule().counts(1, 6))
+        self.assertFalse(Pre2021Rule().counts(2, 0))
+        self.assertFalse(Pre2021Rule().counts(2, 1))
+        self.assertFalse(Pre2021Rule().counts(2, 2))
+        self.assertFalse(Pre2021Rule().counts(2, 3))
+        self.assertFalse(Pre2021Rule().counts(2, 4))
+        self.assertFalse(Pre2021Rule().counts(2, 5))
+
 
 if __name__ == '__main__':
     unittest.main()
