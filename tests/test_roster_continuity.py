8a64e74d8561a233ac346235c5e8b0c4f17469d1
Jury Razumau <jury.razumau@zendesk.com>
2022-11-01 11:26:04 +0100

    fix the first test

--- tests/test_roster_continuity.py
+++ tests/test_roster_continuity.py
@@ -5,7 +5,7 @@ from scripts.roster_continuity import *
 class TestRosterContinuity(unittest.TestCase):
     def test_rule_selection(self):
         old_rule = select_rule(datetime.date(2019, 1, 1))
-        self.assertIs(old_rule, Pre2021Rule)
+        self.assertIs(old_rule, Pre2021Rule())
 
 
 if __name__ == '__main__':
