0cff36d57077ee802e30c9c174454f4dc8a1f14b
Jury Razumau <jury.razumau@zendesk.com>
2022-11-01 11:30:01 +0100

    test rule selection

--- tests/test_roster_continuity.py
+++ tests/test_roster_continuity.py
@@ -3,9 +3,17 @@ from scripts.roster_continuity import *
 
 
 class TestRosterContinuity(unittest.TestCase):
-    def test_rule_selection(self):
+    def test_rule_selection_before_maii(self):
         old_rule = select_rule(datetime.date(2019, 1, 1))
-        self.assertIs(old_rule, Pre2021Rule())
+        self.assertIsInstance(old_rule, Pre2021Rule)
+
+    def test_rule_selection_first_maii_rule(self):
+        late_2021_rule = select_rule(datetime.date(2021, 12, 1))
+        self.assertIsInstance(late_2021_rule, MAIIRule2021to2022)
+
+    def test_rule_selection_second_maii_rule(self):
+        late_2021_rule = select_rule(datetime.date(2022, 12, 1))
+        self.assertIsInstance(late_2021_rule, MAIIRuleFrom2022)
 
 
 if __name__ == '__main__':
