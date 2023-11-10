3d10dd8ee4337576904dd73823a6e2343321df21
Jury Razumau <mail@razumau.net>
2023-11-10 21:38:38 +0100

    Revert "reduce batch size for inserts"
    This reverts commit 01382836416616d1e81ea23098c9633b2b10f1b1.

--- scripts/db_tools.py
+++ scripts/db_tools.py
@@ -16,8 +16,7 @@ def fast_insert(cursor, table: str, columns: str, rows: List[str], schema: str =
     :param rows:
     :return:
     """
-    print(f'Inserting {len(rows)} into {table}')
-    BATCH_SIZE = 500
+    BATCH_SIZE = 50000
     for i in range(0, len(rows), BATCH_SIZE):
         cursor.execute(f'INSERT INTO {schema}.{table} ({columns}) VALUES ' + ', '.join(rows[i:i + BATCH_SIZE]) + ';')
 
