bdb3ad8c57bb3c64f3143d0ce637a5c582b4a2de
Jury Razumau <mail@razumau.net>
2023-11-12 23:32:17 +0100

    use created release for filtering
    Instead of hardcoding an id.

--- tests/test_release.py
+++ tests/test_release.py
@@ -8,7 +8,8 @@ import django
 django.setup()
 
 from scripts.main import calc_release
-from b.models import Team_rating, Tournament_in_release, Player_rating_by_tournament, Player_rating, Tournament_result
+from b.models import (Team_rating, Tournament_in_release, Player_rating_by_tournament,
+                      Player_rating, Tournament_result, Release)
 
 
 class TestReleases(unittest.TestCase):
@@ -16,9 +17,10 @@ class TestReleases(unittest.TestCase):
     def setUpClass(cls):
         release_date = date(2021, 9, 16)
         calc_release(release_date)
+        cls.release = Release.objects.get(date=release_date)
 
     def test_team_rating_values(self):
-        team_at_first_place = Team_rating.objects.filter(release=3).order_by("place")[0]
+        team_at_first_place = Team_rating.objects.filter(release=self.release).order_by("place")[0]
         self.assertEqual(45556, team_at_first_place.team_id)
         self.assertEqual(10737, team_at_first_place.rating)
         self.assertEqual(63, team_at_first_place.rating_change)
@@ -26,7 +28,7 @@ class TestReleases(unittest.TestCase):
         self.assertEqual(1, team_at_first_place.place)
         self.assertEqual(0, team_at_first_place.place_change)
 
-        team_at_place_57 = Team_rating.objects.filter(release=3).order_by("place")[56]
+        team_at_place_57 = Team_rating.objects.filter(release=self.release).order_by("place")[56]
         self.assertEqual(65268, team_at_place_57.team_id)
         self.assertEqual(7653, team_at_place_57.rating)
         self.assertEqual(-82, team_at_place_57.rating_change)
@@ -34,7 +36,7 @@ class TestReleases(unittest.TestCase):
         self.assertEqual(8, team_at_place_57.place_change)
         self.assertEqual(57, team_at_place_57.place)
 
-        team_at_place_400 = Team_rating.objects.filter(release=3).order_by("place")[399]
+        team_at_place_400 = Team_rating.objects.filter(release=self.release).order_by("place")[399]
         self.assertEqual(46627, team_at_place_400.team_id)
         self.assertEqual(5288, team_at_place_400.rating)
         self.assertEqual(0, team_at_place_400.rating_change)
@@ -43,20 +45,20 @@ class TestReleases(unittest.TestCase):
         self.assertEqual(400, team_at_place_400.place)
 
     def test_tournaments_in_release_values(self):
-        tournaments_in_release = Tournament_in_release.objects.filter(release=3)
+        tournaments_in_release = Tournament_in_release.objects.filter(release=self.release)
         self.assertEqual([6044, 6114, 7225, 7325],
                          list(tournaments_in_release.values_list('tournament_id', flat=True)))
 
     def test_player_rating_values(self):
-        player_rating = Player_rating.objects.get(release=3, player_id=77673)
+        player_rating = Player_rating.objects.get(release=self.release, player_id=77673)
         self.assertEqual(12434, player_rating.rating)
         self.assertEqual(33, player_rating.rating_change)
 
     def test_missing_player_rating_values(self):
-        self.assertEqual(0, Player_rating.objects.filter(release=3, player_id=100000).count())
+        self.assertEqual(0, Player_rating.objects.filter(release=self.release, player_id=100000).count())
 
     def test_player_rating_by_tournament_values(self):
-        player_top_bonuses = Player_rating_by_tournament.objects.filter(release=3, player_id=77673)
+        player_top_bonuses = Player_rating_by_tournament.objects.filter(release=self.release, player_id=77673)
         self.assertEqual(7, player_top_bonuses.count())
 
         self.assertEqual(1794, player_top_bonuses.get(tournament_id=7225).cur_score)
