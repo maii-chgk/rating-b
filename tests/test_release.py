9c71f18312dfa59c7769f7e49044b0a91963decf
Jury Razumau <mail@razumau.net>
2023-11-12 22:07:50 +0100

    test values for the correct release
    We recalculate release with id 3, not 2.

--- tests/test_release.py
+++ tests/test_release.py
@@ -12,32 +12,36 @@ from b.models import Team_rating, Tournament_in_release, Player_rating_by_tourna
 
 
 class TestReleases(unittest.TestCase):
-    def test_calc_single_release(self):
+    @classmethod
+    def setUpClass(cls):
         release_date = date(2021, 9, 16)
         calc_release(release_date)
 
-        team_at_first_place = Team_rating.objects.filter(release=2).order_by("place")[0]
+    def test_team_rating_values(self):
+        team_at_first_place = Team_rating.objects.filter(release=3).order_by("place")[0]
         self.assertEqual(45556, team_at_first_place.team_id)
-        self.assertEqual(10674, team_at_first_place.rating)
-        self.assertEqual(21, team_at_first_place.rating_change)
+        self.assertEqual(10737, team_at_first_place.rating)
+        self.assertEqual(63, team_at_first_place.rating_change)
 
-        team_at_place_53 = Team_rating.objects.filter(release=2).order_by("place")[52]
-        self.assertEqual(50186, team_at_place_53.team_id)
-        self.assertEqual(7697, team_at_place_53.rating)
-        self.assertEqual(-189, team_at_place_53.rating_change)
+        team_at_place_57 = Team_rating.objects.filter(release=3).order_by("place")[56]
+        self.assertEqual(65268, team_at_place_57.team_id)
+        self.assertEqual(7653, team_at_place_57.rating)
+        self.assertEqual(-82, team_at_place_57.rating_change)
 
-        team_at_place_400 = Team_rating.objects.filter(release=2).order_by("place")[399]
+        team_at_place_400 = Team_rating.objects.filter(release=3).order_by("place")[399]
         self.assertEqual(46627, team_at_place_400.team_id)
         self.assertEqual(5288, team_at_place_400.rating)
         self.assertEqual(0, team_at_place_400.rating_change)
 
-        tournaments_in_release = Tournament_in_release.objects.filter(release=2)
-        self.assertEqual([6560, 6639, 7086, 7182, 7513],
+    def test_tournaments_in_release_values(self):
+        tournaments_in_release = Tournament_in_release.objects.filter(release=3)
+        self.assertEqual([6044, 6114, 7225, 7325],
                          list(tournaments_in_release.values_list('tournament_id', flat=True)))
 
-        players_in_release = Player_rating_by_tournament.objects.filter(release=2)
-        self.assertEqual(2264, players_in_release.get(player_id=80897, tournament_id=6639).cur_score)
-        self.assertEqual(2264, players_in_release.get(player_id=80897, tournament_id=6639).initial_score)
-        self.assertEqual(1767, players_in_release.get(player_id=80897, tournament_id=6076).cur_score)
-        self.assertEqual(1934, players_in_release.get(player_id=80897, tournament_id=6076).initial_score)
-        self.assertEqual(9, players_in_release.get(player_id=80897, tournament_id=6076).weeks_since_tournament)
+    def test_player_rating_by_tournament_values(self):
+        players_in_release = Player_rating_by_tournament.objects.filter(release=3)
+        self.assertEqual(1794, players_in_release.get(player_id=77673, tournament_id=7225).cur_score)
+        self.assertEqual(1794, players_in_release.get(player_id=77673, tournament_id=7225).initial_score)
+        self.assertEqual(1933, players_in_release.get(player_id=77673, tournament_id=5923).cur_score)
+        self.assertEqual(2053, players_in_release.get(player_id=77673, tournament_id=5923).initial_score)
+        self.assertEqual(6, players_in_release.get(player_id=77673, tournament_id=5923).weeks_since_tournament)
